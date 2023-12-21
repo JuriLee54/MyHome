e3e2e64d28e27eefc2e8633982bd9f28ef41a4ad
wnf77 <wnf7777@naver.com>
2023-12-21 13:39:21 +0900

    웹사이트 구축

--- js/my_script.js
+++ js/my_script.js
@@ -1,128 +1,59 @@
-function init(){
-    var t1 = document.getElementById("t1");
-    var t2 = document.getElementById("t2");
-    var t3 = document.getElementById("t3");
-    var t4 = document.getElementById("t4");
-    var t5 = document.getElementById("t5");
-    var t6 = document.getElementById("t6");
-    var t7 = document.getElementById("t7");
-    var t8 = document.getElementById("t8");
+function clickTable1(){
+    var t1 = document.getElementById("t1").innerHTML;
+    var a1 = document.getElementById("a1");
+    var lap = document.getElementById("lap");
 
-    var a1=document.getElementById("a1");
-    var a2=document.getElementById("a2");
-    var a3=document.getElementById("a3");
-    var a4=document.getElementById("a4");
-    var a5=document.getElementById("a5");
-    var a6=document.getElementById("a6");
-    var a7=document.getElementById("a7");
-    var a8=document.getElementById("a8");
-
-        t1.style.display = "block";
-        t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "none";
-
-    a1.onclick = function (){
-       t1.style.display = "block";
-       t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "none";
-    }
-
-    a2.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "block";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "none";
-    }
-
-    a3.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "none";
-        t3.style.display = "block";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "none";
-    }
-
-    a4.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "block";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "none";
-    }
-
-    a5.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "block";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "none";
-    }
-
-    a6.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "block";
-        t7.style.display = "none";
-        t8.style.display = "none";
-    }
-
-    a7.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "block";
-        t8.style.display = "none";
-    }
-
-    a8.onclick = function (){
-        t1.style.display = "none";
-        t2.style.display = "none";
-        t3.style.display = "none";
-        t4.style.display = "none";
-        t5.style.display = "none";
-        t6.style.display = "none";
-        t7.style.display = "none";
-        t8.style.display = "block";
-    }
-
-    var pre = document.querySelector("#pre");
-    var next = document.querySelector("#next");
-
-
+    lap.innerHTML = t1;
 }
-
 function tool() {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')) ;
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     });
 }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Function to load table HTML content into the lap div
+    function loadTable(tableNumber) {
+      // Assuming your table URLs follow the pattern "../reports/tableX.html"
+      var tableURL = "../reports/table" + tableNumber + ".html";
+
+      // Using fetch to load HTML content from the specified URL
+      fetch(tableURL)
+        .then((response) => response.text())
+        .then((html) => {
+          // Setting the innerHTML of the lap div with the loaded HTML content
+          document.getElementById("lap").innerHTML = html;
+        })
+        .catch((error) => {
+          console.error("Error loading table:", error);
+        });
+    }
+
+    // Call loadTable with table number 1 to initially show the first table
+    loadTable(1);
+
+    // Adding click event listeners to the pagination links
+    for (var i = 1; i <= 8; i++) {
+      var linkId = "a" + i;
+      document.getElementById(linkId).addEventListener("click", function (event) {
+        // Preventing the default behavior of the link
+        event.preventDefault();
+        // Extracting the table number from the link's href attribute
+        var tableNumber = event.target.getAttribute("href").match(/\d+/)[0];
+        // Calling the loadTable function with the extracted table number
+        loadTable(tableNumber);
+      });
+    }
+
+    // Adding click event listeners to the page buttons
+    var pageButtons = document.querySelectorAll(".page-item");
+    pageButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        // Extracting the page number from the button's text content
+        var pageNumber = button.textContent.trim();
+        // Calling the loadTable function with the extracted page number
+        loadTable(pageNumber);
+      });
+    });
+  });
